4126b3bd875ae7f6af869a410255941256bcad2f
Aleksandr <116078256+LabeUri@users.noreply.github.com>
2024-01-22 11:05:26 +0200

    Add files via upload

---
+++ 5robota.py
@@ -0,0 +1,3 @@
+num1, num2, num3 = map(float, input("Напишіть\ три числа через пробіл: ").split())
+print(f"найменше число: {min(num1, num2, num3)}")
+print(f"найбільше число: {max(num1, num2, num3)}")
